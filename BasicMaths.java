393880fa69c8b6985e41a1cb0f7e5aa32d21f1e3
Rajesh <rajeshranjan8271@gmail.com>
2024-09-23 13:12:10 +0530

    trailing zeroes remains

--- BasicMaths.java
+++ BasicMaths.java
@@ -1,10 +1,21 @@
+import java.util.Arrays;
+
 public class BasicMaths {
     public static void main(String[] args) {
-        countDigits(657689);
-        countDigitsEfficient(657689);
-        findArmstrong(373);
-        checkPalindrome(121);
-        printAllDivisors(13);
+        // countDigits(657689);
+        // countDigitsEfficient(657689);
+        // findArmstrong(373);
+        // checkPalindrome(121);
+        // printAllDivisors(13);
+        // checkPrime(37);
+        // sieveOfEratosthenes(37);
+        // System.out.println((int)newtonRaphsonAlgo(25));
+        // gcd(0, 5);
+        // euclideanGCD(24, 36);
+        // System.out.println("GCD : " + efficientEuclideanGCD(24, 36));
+        // lcm(24, 36);
+        factorial(5);
+        
     }
 
     static void countDigits(int num) {
@@ -26,7 +37,8 @@ public class BasicMaths {
             System.out.println(1);
             return;
         }
-        int counter = (int) ((int) Math.log(num) / Math.log(10)) + 1;
+        // int counter = (int) ((int) Math.log(num) / Math.log(10)) + 1;
+        int counter = (int)Math.log10(num) + 1;
         System.out.println(counter);
     }
 
@@ -59,7 +71,7 @@ public class BasicMaths {
     }
 
     static void printAllDivisors(int num) {
-        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+        for (int i = 2; i <= (int) newtonRaphsonAlgo(num); i++) {
             if (num % i == 0) {
                 System.out.println(i + " : is divisor of " + num);
                 int temp = num / i;
@@ -68,5 +80,112 @@ public class BasicMaths {
                 }
             }
         }
+    }
+
+    static void checkPrime(int num) {
+        if (num == 0 || num == 1) {
+            System.out.println(num + " is neither a prime nor a composite number.");
+            return;
+        }
+        for (int i = 2; i <= (int) newtonRaphsonAlgo(num); i++) {
+            if (num % i == 0) {
+                System.out.println(num + " is a composite number.");
+                return;
+            }
+        }
+        System.out.println(num + " is a prime number.");
+    }
+    
+    static void sieveOfEratosthenes(int num) {
+        boolean arr[] = new boolean[num + 1];
+        Arrays.fill(arr, true);
+        int counter = 2;
+        while (counter <= num) {
+            if (arr[counter] == true) {
+                for (int factor = counter + counter; factor <= num; factor += counter) {
+                    arr[factor] = false;
+                }
+            }
+            counter++;
+        }
+        for (int i = 2; i <= num; i++) {
+            if (arr[i] == true) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println("are prime numbers till " + num + ".");
+    }
+
+    static double newtonRaphsonAlgo(int num) {
+        double root;
+        double X = num;
+        double tolerance = 0.00001;
+
+        while (true) {
+            root = 0.5 * (X + (num / X));
+            double ans = X - root;
+            if (ans < tolerance) {
+                break;
+            }
+            X = root;
+        }
+        return X;
+    }
+
+    static void gcd(int num1, int num2) {
+        int min = (num1 > num2) ? num2 : num1;
+        while (min > 0) {
+            if (num1 % min == 0 && num2 % min == 0) {
+                System.out.println("GCD of " + num1 + " and " + num2 + " : " + min);
+                return;
+            }
+            min--;
+        }
+        int max = (num1 > num2) ? num1 : num2;
+        System.out.println("GCD of " + num1 + " and " + num2 + " : " + max);
+    }
+
+    static void euclideanGCD(int num1, int num2) {
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) {
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+        }
+        int res = num1;
+        if (num1 == 0) {
+            res = num2;
+        }
+        System.out.println("GCD : " + res);
+    }
+
+    static int efficientEuclideanGCD(int num1, int num2) {
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) {
+                num1 %= num2;
+            } else {
+                num2 %= num1;
+            }
+        }
+        int res = num1;
+        if (num1 == 0) {
+            res = num2;
+        }
+        return res;
+    }
+
+    static void lcm(int num1, int num2) {
+        int res = (num1 * num2) / efficientEuclideanGCD(num1, num2);
+        System.out.println("LCM of " + num1 + " and " + num2 + " : " + res);
+    }
+
+    static void factorial(int num) {
+        long res = 1;
+        while (num > 1) {
+            res *= num;
+            num--;
+        }
+        System.out.println("Factorial of num : " + res);
     }
 }
